2e2eccb93b6679a989ca881ba89815bde5aae2cf
Полковниковаав <@208-05>
2022-11-28 13:20:01 +0300

    Добавьте файлы проекта.

---
+++ Hotel2/Avtoriz.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hotel2
+{
+    /// <summary>
+    /// Логика взаимодействия для Avtoriz.xaml
+    /// </summary>
+    public partial class Avtoriz : Page
+    {
+        public Avtoriz()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_AvtorizHome(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Home.xaml", UriKind.Relative));    //Переход на страницу Home (Button)
+        }
+
+        private void btn_AvtorizRegistr(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Registr.xaml", UriKind.Relative));
+        }
+    }
+}
