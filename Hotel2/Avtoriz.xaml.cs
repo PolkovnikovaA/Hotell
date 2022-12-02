bc503694bf9b94cb5631df6a6830e1e027bc58df
Anastasiy1307 <anastasiy131100714@gmail.com>
2022-12-02 12:58:31 +0300

    Basa

--- Hotel2/Avtoriz.xaml.cs
+++ Hotel2/Avtoriz.xaml.cs
@@ -25,9 +25,47 @@ namespace Hotel2
             InitializeComponent();
         }
 
+        List<Hotel2.Registr> users = new List<Hotel2.Registr>();
         private void btn_AvtorizHome(object sender, RoutedEventArgs e)
         {
-            NavigationService.Navigate(new Uri("/Home.xaml", UriKind.Relative));    //Переход на страницу Home (Button)
+            string login_vx = TextBoxLogin.Text;
+            string password_vx = PasswordBox.Password;
+            int count = Entities.GetContext().Registr.Count();
+            users = Entities.GetContext().Registr.ToList();
+
+            for (int i = 0; i < count; i++)
+            {
+                if (users[i].Login == login_vx)
+                {
+                    if (users[i].Password == password_vx)
+                    {
+                        NavigationService.Navigate(new Uri("/Home.xaml", UriKind.Relative));    //Переход на страницу Home (Button)
+                    }
+                    else
+                    {
+                        MessageBox.Show("Неверный пароль");
+                    }
+                }
+            }
+
+            //if (login_vx != "") // проверяем введён ли логин     
+            //{
+            //    if (password_vx != "") // проверяем введён ли пароль         
+            //    {             // ищем в базе данных пользователя с такими данными         
+            //        DataTable dt_user = Select("SELECT * FROM Reg WHERE Login = '" + TextBoxLog.Text + "' AND Password = '" + TextBoxpass.Password + "'");
+            //        if (dt_user.Rows.Count > 0) // если такая запись существует       
+            //        {
+            //            MainWindow mainWindow = new MainWindow();
+            //            mainWindow.Show();
+            //            Close();
+            //        }
+            //        else MessageBox.Show("Пользователя не найден"); // выводим ошибку  
+            //    }
+            //    else MessageBox.Show("Введите пароль"); // выводим ошибку    
+            //}
+            //else MessageBox.Show("Введите логин"); // выводим ошибку 
+
+            
         }
 
         private void btn_AvtorizRegistr(object sender, RoutedEventArgs e)
